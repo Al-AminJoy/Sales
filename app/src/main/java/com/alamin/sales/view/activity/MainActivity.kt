8346e5ab4b11283c53492404a21c1f7a55c9eabe
Al-AminJoy <alaminislam3555@gmail.com>
2022-07-28 04:10:20 +0600

    Track Location

--- app/src/main/java/com/alamin/sales/view/activity/MainActivity.kt
+++ app/src/main/java/com/alamin/sales/view/activity/MainActivity.kt
@@ -1,25 +1,161 @@
 package com.alamin.sales.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.navigation.Navigation.findNavController
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.alamin.sales.R
 import com.alamin.sales.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-private lateinit var binding: ActivityMainBinding
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+        getLocationData()
         setupActionBarWithNavController(findNavController(R.id.fragment));
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        getLocationData()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragment);
         return super.onSupportNavigateUp() || navController.navigateUp();
+    }
+
+    private fun getLocationData() {
+        if (checkPermission()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    location?.let {
+                        Toast.makeText(
+                            applicationContext,
+                            "Location Received Lat: ${location.latitude} Long: ${location.longitude}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.turn_on_location),
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermission();
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false;
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocationData()
+            } else {
+                val isNeverFine =
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+                val isNeverCoarse =
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+                val isneverBackground =
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                if (!isNeverCoarse && !isNeverFine && !isneverBackground) {
+                    showAlertDialog(true)
+                } else {
+                    showAlertDialog(false)
+                }
+
+            }
+        }
+    }
+
+    private fun manualPermission() {
+        startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        })
+    }
+
+    private fun showAlertDialog(isPermanent: Boolean) {
+        AlertDialog.Builder(this)
+            .setTitle("Permission Needed")
+            .setMessage("Permission is needed to access location from your device...")
+            .setCancelable(false)
+            .setPositiveButton(
+                "OK"
+            ) { dialog, which ->
+                if (isPermanent) manualPermission() else requestPermission()
+            }
+            .create().show()
     }
 }
--- app/src/main/java/com/alamin/sales/view/fragment/AttendanceFragment.kt
+++ app/src/main/java/com/alamin/sales/view/fragment/AttendanceFragment.kt
@@ -1,17 +1,29 @@
 package com.alamin.sales.view.fragment
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
 import com.alamin.sales.R
 import com.alamin.sales.databinding.FragmentAttendanceBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 private const val TAG = "AttendanceFragment"
+
 class AttendanceFragment : Fragment() {
 
     private lateinit var binding: FragmentAttendanceBinding
@@ -21,7 +33,14 @@ class AttendanceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAttendanceBinding.inflate(layoutInflater)
-        Log.d(TAG, "onCreateView: ${args.store}")
+
         return binding.root
     }
+
+
+
+
+
+
+
 }
